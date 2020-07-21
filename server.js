f7e11398755fd368846ad3a42afeaacbd28a29cb
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-07-21 08:03:31 -0400

    Setup server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const PORT = 8000;
+
+//res.status(500).json({ message: 'Server error', error: error });
+
+server
+	.post('/api/register', (req, res) => {
+        res.json({message: 'ok!'});
+    });
+
+server
+	.post('/api/login', (req, res) => {});
+
+server
+	.get('/api/users', (req, res) => {});
+
+server.listen(PORT, () => {
+	console.log(`Server is running on ${PORT}`);
+});
